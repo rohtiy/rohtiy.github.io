bc0d8ba6868302be210617188d159507fa07a417
Rohit <rohityadav74654@gmail.com>
2026-04-03 19:36:25 +0530

    Update Favicon;

---
+++ experiments/events/scripts/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('load', () => {
+
+    let pet = "";
+    pet.style.backgroundColor = "red";
+    pet.width = 100;
+    pet.height = 100;
+    pet.style.position = "absolute";
+
+    let petSpeed = 1;
+
+    document.appendChild(pet);
+
+    function startFollow(pet) {
+        pet.top += petSpeed;
+        pet.right += petSpeed;
+    }
+   
+    requestAnimationFrame(startFollow());
+})
