b1b9204ebb80618b6e86c5a687a1b8b8042fe04a
Rohit <rohityadav74654@gmail.com>
2026-04-06 20:11:07 +0530

    Falling Stars Effect

---
+++ experiments/falling-stars/scripts/index.js
@@ -0,0 +1,94 @@
+import { WindowResize, KeyListener, MyCanvas } from "../../../scripts/index.js";
+
+const canvasInstance = new MyCanvas({ context: '2d', size: { width: window.innerWidth, height: window.innerHeight } });
+new KeyListener({ event: 'keydown', element: canvasInstance.canvas });
+
+new WindowResize({
+    callback: () => {
+        canvasInstance.canvas.width = window.innerWidth;
+        canvasInstance.canvas.height = window.innerHeight
+    }
+});
+
+
+const initialParticles = {
+    layerOne: {
+        color: 'white',
+        particles: [],
+        count: 100,
+        radius: 3,
+        speed: 8,
+    },
+    layerTwo: {
+        color: 'white',
+        particles: [],
+        count: 100,
+        radius: 2,
+        speed: 5,
+    },
+    layerThree: {
+        color: 'white',
+        particles: [],
+        count: 100,
+        radius: 1,
+        speed: 2,
+    }
+}
+
+function drawParticles(number, colour) {
+    for (let i = 0; i < number; i++) {
+        let center = {
+            x: Math.random() * canvasInstance.canvas.width,
+            y: Math.random() * canvasInstance.canvas.height
+        }
+        canvasInstance.drawCircle(colour, center, 2);
+    }
+}
+
+function animate() {
+    canvasInstance.fillScreen();
+    drawParticles(500, 'grey');
+    requestAnimationFrame(animate);
+}
+
+
+function getRandomPositionOnCanvas() {
+    return {
+        x: Math.random() * canvasInstance.canvas.width,
+        y: Math.random() * canvasInstance.canvas.height
+    }
+}
+
+
+function getNextFrame() {
+    canvasInstance.fillScreen('black');
+    for (let layerKey in initialParticles) {
+        const { particles, count, radius, speed, color } = initialParticles[layerKey];
+        for (let i = 0; i < count; i++) {
+            particles[i] = {
+                x: particles[i].x,
+                y: (particles[i].y + speed) > canvasInstance.canvas.height ? 0 : particles[i].y + speed,
+            }
+            canvasInstance.drawCircle(color, particles[i], radius);
+        }
+    }
+    requestAnimationFrame(getNextFrame);
+}
+
+
+function initialParticleSetup() {
+    for (let layerKey in initialParticles) {
+        const { particles, count } = initialParticles[layerKey];
+        for (let i = 0; i < count; i++) {
+            let particle = getRandomPositionOnCanvas();
+            particles.push(particle);
+        }
+    }
+}
+
+function setup() {
+    initialParticleSetup();
+    getNextFrame();
+}
+
+setup();
